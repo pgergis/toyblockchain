39b2334bb828e05de86f40e59d8a8ab39a5cf268
Pierre Gergis <pierre.s.gergis@gmail.com>
2018-08-29 17:42:33 -0400

    unfinished tests

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -25,6 +25,20 @@ class TestBlockchain (unittest.TestCase):
 
         self.assertTrue(blockchain.valid_transaction(tx1))
 
+    def test_double_pay_transaction_fail(self):
+        blockchain = Blockchain()
+        wallet1 = Wallet()
+        wallet2 = Wallet()
+        initial_tx = Transaction(0, wallet1.public_key(), None)
+        blockchain.new_block([initial_tx], 0, 0)
+
+        tx = Transaction(wallet1.public_key(), wallet2.public_key(), initial_tx)
+        blockchain.new_block([tx], 0, 0)
+        self.assertTrue(blockchain.valid_transaction(tx))
+        tx2 = Transaction(wallet1.public_key(), wallet2.public_key(), initial_tx)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
